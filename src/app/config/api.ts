d0696d723808ee42e052bdd4ec8dde696fa32fc5
Santino <mellamoalroberto@gmail.com>
2025-08-13 14:29:14 -0300

    icon

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -1,4 +1,4 @@
-export const GET_CURRENCIES_API = 'http://localhost:8080/api/v1/currency';
-export const GET_DOLLARS_API = 'http://localhost:8080/api/v1/currency/dollars';
-export const GET_DOLLARS_YESTERDAY_API = 'http://localhost:8080/api/v1/currency/dollars/yesterday';
-export const CONVERT_CURRENCIES_API = 'http://localhost:8080/api/v1/currency/convert';
+export const GET_CURRENCIES_API = 'https://dollars-api-spring.onrender.com/api/v1/currency';
+export const GET_DOLLARS_API = 'https://dollars-api-spring.onrender.com/api/v1/currency/dollars';
+export const GET_DOLLARS_YESTERDAY_API = 'https://dollars-api-spring.onrender.com/api/v1/currency/dollars/yesterday';
+export const CONVERT_CURRENCIES_API = 'https://dollars-api-spring.onrender.com/api/v1/currency/convert';
